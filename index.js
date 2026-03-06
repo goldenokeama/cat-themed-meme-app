3943714da07fc71af4dfa4b4675ce7eb25cc2b8f
Golden1636 <okeamagolden@yahoo.com>
2026-03-06 08:09:22 +0100

    implemented a function called getMatchingCatsArray that returns an array of all of the cats that match the user's criteria

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-import { catsData } from "/data.js";
+import { catsData } from '/data.js'
 
-const emotionRadios = document.getElementById("emotion-radios");
+const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
 
-emotionRadios.addEventListener("change", highlightCheckedOption);
+emotionRadios.addEventListener('change', highlightCheckedOption)
 
-function highlightCheckedOption(e) {
-  const radios = document.getElementsByClassName("radio");
+getImageBtn.addEventListener('click', getMatchingCatsArray)
 
-  for (let radio of radios) {
-    radio.classList.remove("highlight");
-  }
-
-  document.getElementById(e.target.id).parentElement.classList.add("highlight");
-}
-
-function getEmotionsArray(cats) {
-  const emotionsArray = [];
-  for (let cat of cats) {
-    for (let emotion of cat.emotionTags) {
-      if (!emotionsArray.includes(emotion)) {
-        emotionsArray.push(emotion);
-      }
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    for (let radio of radios){
+        radio.classList.remove('highlight')
     }
-  }
-  return emotionsArray;
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
 
-function renderEmotionsRadios(cats) {
-  let radioItems = ``;
-  const emotions = getEmotionsArray(cats);
-  for (let emotion of emotions) {
-    radioItems += `
+function getMatchingCatsArray(){     
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+        const isGif = gifsOnlyOption.checked
+        
+        const matchingCatsArray = catsData.filter(function(cat){
+            if(isGif){
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+            }
+            else{
+                return cat.emotionTags.includes(selectedEmotion)
+            }
+            
+        })
+        return matchingCatsArray
+    }  
+}
+
+function getEmotionsArray(cats){
+    const emotionsArray = []    
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
+            if (!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion)
+            }
+        }
+    }
+    return emotionsArray
+}
+
+
+function renderEmotionsRadios(cats){
+        
+    let radioItems = ``
+    const emotions = getEmotionsArray(cats)
+    for (let emotion of emotions){
+        radioItems += `
         <div class="radio">
             <label for="${emotion}">${emotion}</label>
             <input
@@ -39,9 +61,13 @@ function renderEmotionsRadios(cats) {
             value="${emotion}"
             name="emotions"
             >
-        </div>`;
-  }
-  emotionRadios.innerHTML = radioItems;
+        </div>`
+    }
+    emotionRadios.innerHTML = radioItems
 }
 
-renderEmotionsRadios(catsData);
+renderEmotionsRadios(catsData)
+
+
+
+
